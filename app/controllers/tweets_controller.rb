fc744d17dcadfe78cb4300d5e05c5991e0f2d452
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-07 19:50:49 +0900

    確認画面機能追加(投稿画面に戻った時打ち込んだ内容が保持される)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,23 +1,13 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
-
-  # GET /tweets
-  # GET /tweets.json
   def index
     @tweets = Tweet.all
   end
-
-  # GET /tweets/1
-  # GET /tweets/1.json
   def show
   end
-
-  # GET /tweets/new
   def new
     @tweet = Tweet.new
   end
-
-  # GET /tweets/1/edit
   def edit
   end
 
@@ -25,20 +15,20 @@ class TweetsController < ApplicationController
   # POST /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
-
-    respond_to do |format|
-      if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
-      else
-        format.html { render :new }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+    if params[:back]
+      render :new
+    else
+      respond_to do |format|
+        if @tweet.save
+          format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+          format.json { render :show, status: :created, location: @tweet }
+        else
+          format.html { render :new }
+          format.json { render json: @tweet.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
-
-  # PATCH/PUT /tweets/1
-  # PATCH/PUT /tweets/1.json
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
@@ -50,9 +40,6 @@ class TweetsController < ApplicationController
       end
     end
   end
-
-  # DELETE /tweets/1
-  # DELETE /tweets/1.json
   def destroy
     @tweet.destroy
     respond_to do |format|
@@ -60,14 +47,14 @@ class TweetsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  def confirm
+    @tweet = Tweet.new(tweet_params)
+    render :new if @tweet.invalid?
+  end
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_tweet
       @tweet = Tweet.find(params[:id])
     end
-
-    # Only allow a list of trusted parameters through.
     def tweet_params
       params.require(:tweet).permit(:content)
     end
